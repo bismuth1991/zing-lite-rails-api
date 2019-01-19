fbeb63226c0443a909a7dd2b921db04c6115ec15
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-19 12:46:11 -0500

    Write password encryption test for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'password encryption' do
+    it 'does not save passwords to the database' do
+      User.create!(username: 'chuck_norris', password: 'password')
+      user = User.find_by_username('chuck_norris')
+      expect(user.password).not_to be('password')
+    end
+
+    it 'encrypts the password using BCrypt' do
+      expect(BCrypt::Password).to receive(:create)
+      User.new(username: 'chuck_norris', password: 'password')
+    end
+  end
 end
