039280bc99acf6242e2fc4905f2daf10aceba3da
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-19 03:14:53 -0500

    Write validation tests for song model & association test between song/artist/album

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  # it { should have_many(:songs) }
+  it { should have_many(:songs) }
   
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:avatar_url) }
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:artist) }
+  it { should belong_to(:album) }
+
+  it { should validate_presence_of(:title) }
 end
