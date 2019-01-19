094f3aa88bbe9f5ca31ad75266e1824158f0ef29
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-19 03:18:33 -0500

    Add url column to songs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_081207) do
+ActiveRecord::Schema.define(version: 2019_01_19_081744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_01_19_081207) do
     t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
     t.index ["album_id"], name: "index_songs_on_album_id"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
     t.index ["title"], name: "index_songs_on_title"
