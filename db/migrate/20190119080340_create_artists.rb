9b9e2c31fcb937b6764a0c766a62b10fd91742f8
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-19 03:05:27 -0500

    Create artists table

---
+++ db/migrate/20190119080340_create_artists.rb
@@ -0,0 +1,12 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :name, null: false
+      t.string :avatar_url, null: false
+    
+      t.timestamps
+    end
+
+    add_index :artists, :name
+  end
+end
