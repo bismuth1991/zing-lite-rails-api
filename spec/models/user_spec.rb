3e6f55551940183b46f590b749af9ea79f2cce12
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-19 12:52:13 -0500

    Add uniqueness validation for username in User model, prepare to migrate users table

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,5 +23,6 @@ RSpec.describe User, type: :model do
 
   it { should validate_length_of(:password).is_at_least(6) }
   it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
   it { should validate_presence_of(:password_digest) }
 end
