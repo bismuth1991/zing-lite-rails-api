a96b811ceffd287304a256dc9be8c97e4e96e6fc
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-19 03:10:34 -0500

    Write validation tests for albums table

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # it { should have_many(:songs) }
+  
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:cover_image_url) }
 end
