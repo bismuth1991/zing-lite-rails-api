7a738f67a6b0ab660002f39d8a3952dcc12f1d13
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-20 13:27:01 -0500

    Add POST and DELETE routes for sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
+    resource :session, only: [:create, :destroy]
+    
     resources :songs, only: [:index]  
   end 
 end
