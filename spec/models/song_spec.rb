8ddab0848e92d1a1528f51a999270796ece1deee
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-27 13:30:34 -0500

    Create playlist model, pass specs

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Song, type: :model do
   it { should belong_to(:artist) }
   it { should belong_to(:album) }
 
+  it { should have_many(:playlists) }
+
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:url) }
 end
