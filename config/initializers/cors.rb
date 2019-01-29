0fb93bc4f7cf143ed504957f2b822812de17f925
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-29 08:45:36 -0800

    Enable cors for github page

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000, https://bismuth1991.github.io/zing-lite'
 
     resource '*',
       headers: :any,
